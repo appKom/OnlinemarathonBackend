ed653f586c612cf07353d133cf8f81da3d1b566d
Aksel Fosaas <63968427+akselsf@users.noreply.github.com>
2023-03-20 20:19:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
-require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
