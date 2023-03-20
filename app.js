d67f2e40f09d2ede26553ca0fcff92a4a33f4202
Aksel Fosaas <63968427+akselsf@users.noreply.github.com>
2023-03-20 19:21:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fetch = (...args) =>
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const fileName = "./token.json";
 
 let stravadata;
