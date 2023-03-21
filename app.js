73debadcd66d5490607af834bfb35bc0eb1b79bc
Aksel Skullerud Fosaas <aksel.s.fosaas@gmail.com>
2023-03-21 12:43:20 +0100

    Lagre tokens til fil etter første hent fra env

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ try {
 } catch {}
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
-
 const express = require("express");
+const fs = require("fs");
+const filename = "./token.json";
 
 const app = express();
 const port = process.env.PORT || 3000;
+let lastFetched;
+
 app.use(function (req, res, next) {
   const allowedOrigins = ["http://localhost:3000"];
   const origin = req.headers.origin;
@@ -41,6 +44,9 @@ function startInterval() {
 }
 
 async function getStrava() {
+  if (data.expires_at < Date.now() / 1000) {
+    await refreshToken().then();
+  }
   await fetch(
     "https://www.strava.com/api/v3/clubs/1118846/activities?access_token=" +
       data.access_token
@@ -54,7 +60,7 @@ async function getStrava() {
 function formatStravaData(data) {
   let res = [];
   let indexes = {};
-  console.log(data);
+  console.log("Formatting data");
   try {
     data.forEach((d) => {
       if (
@@ -77,7 +83,7 @@ function formatStravaData(data) {
       }
     });
   } catch {
-    console.log(data);
+    console.log("Error");
   }
   return res;
 }
@@ -90,6 +96,15 @@ function logTokens() {
 }
 
 async function refreshToken() {
+  console.log(
+    data.client_id +
+      " " +
+      data.client_secret +
+      " " +
+      data.refresh_token +
+      " " +
+      "refresh_token"
+  );
   return await fetch("https://www.strava.com/oauth/token", {
     method: "POST",
     headers: {
@@ -108,7 +123,7 @@ async function refreshToken() {
         data.access_token = r.access_token;
         data.refresh_token = r.refresh_token;
         data.expires_at = r.expires_at;
-        console.log("REFRESHED TOKENS");
+
         logTokens();
       } else {
         console.log("FAILED TO REFRESH TOKENS");
@@ -118,20 +133,64 @@ async function refreshToken() {
     });
 }
 
+async function saveToFile(data) {
+  fs.writeFile(filename, JSON.stringify(data), (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Saved to file");
+    }
+  });
+}
+
 app.get("/", (req, res) => {
-  res.send(stravadata);
+  if (Date.now() - lastFetched > 1000 * 60) {
+    getStrava().then(() => {
+      lastFetched = Date.now();
+      res.send(stravadata);
+    });
+  } else {
+    res.send(stravadata);
+  }
 });
 
 app.listen(port, () => {
-  data.access_token = process.env.ACCESS_TOKEN;
-  data.refresh_token = process.env.REFRESH_TOKEN;
-  data.expires_at = process.env.EXPIRES_AT;
-  data.client_secret = process.env.CLIENT_SECRET;
-  data.client_id = process.env.CLIENT_ID;
+  fs.readFile(filename, "utf8", async (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    lastFetched = 0;
+    result = JSON.parse(result);
+    if (result.access_token) {
+      console.log("-----------\nGot secrets from file: ");
+      console.log(result);
+      console.log("-----------");
+      data.access_token = result.access_token;
+      data.refresh_token = result.refresh_token;
+      data.expires_at = result.expires_at;
+      data.client_secret = result.client_secret;
+      data.client_id = result.client_id;
+    } else {
+      console.log("--------\nGot secrets from env: ");
 
-  refreshToken().then(getStrava()).then(startInterval());
+      console.log("Access:" + process.env.ACCESS_TOKEN);
+      console.log("Refresh: " + process.env.REFRESH_TOKEN);
+      console.log("Exp at:" + process.env.EXPIRES_AT);
+      console.log("Client id: " + process.env.CLIENT_ID);
+      console.log("Client secret" + process.env.CLIENT_SECRET);
+      console.log("-----------");
+      data.access_token = process.env.ACCESS_TOKEN;
+      data.refresh_token = process.env.REFRESH_TOKEN;
+      data.expires_at = process.env.EXPIRES_AT;
+      data.client_secret = process.env.CLIENT_SECRET;
+      data.client_id = process.env.CLIENT_ID;
+    }
+
+    saveToFile(data).then(
+      refreshToken().then(getStrava()).then(startInterval())
+    );
+  });
 
   console.log(`Port: ${port}`);
-
-  logTokens();
 });
