dfd69d2c4b02314c4b579b61bf576ed3a316ef5f
Aksel Fosaas <akselfosaas@Aksels-MacBook-Pro-2.local>
2023-09-05 18:01:56 +0200

    reinstall yarn

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 mongoose.set("strictQuery", false);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const DBURL = process.env.DBURL;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
